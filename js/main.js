8c27de83bff08c4c326dcb199a07f43291c374a1
Matt Bell <mappum@gmail.com>
2016-01-20 19:05:48 -0800

    Removed genetic mutation stuff

--- js/main.js
+++ js/main.js
@@ -1,96 +1,17 @@
-var GENERATION_SIZE = 50;
-var BEST_SIZE = 10;
-var TIMEOUT = 300;
-var SPEED_WEIGHT = 8000;
-var WORKERS = 3;
-var MUTATION_RATE = 0.25;
-var LENGTH = 20;
+window.onload = function () {
+  qwop.load()
 
-var generation = [];
-var nGen = +localStorage.generations || 0;
-var iframes = [];
-
-var best = [];
-if(localStorage.best) {
-  best = JSON.parse(localStorage.best);
-  for(var i = 0; i < best.length; i++) {
-    best[i].genotype = new Genotype(best[i].genotype);
+  qwop.onReady = function () {
+    console.log('ready')
   }
-  console.log('Loaded best sequences from localStorage');
-}
 
-function populate() {
-  for(var i = 0; i < GENERATION_SIZE; i++) {
-    if(best.length > 1) {
-      var p1, p2;
-      p1 = best[Math.random() * best.length | 0];
-      do {
-        p2 = best[Math.random() * best.length | 0];
-      } while(p1 === p2)
-      generation.push(p1.genotype.breed(p2.genotype, MUTATION_RATE));
-    } else {
-      var g = new Genotype(LENGTH);
-      g.randomize();
-      generation.push(g);
-    }
+  qwop.onStart = function () {
+    console.log('start')
+  }
+
+  qwop.onFrame = function () {}
+
+  qwop.onDeath = function () {
+    console.log('death')
   }
 }
-
-function updateBest() {
-  best.sort(function(a, b) {
-    return a.score > b.score ? 1 : -1;
-  });
-  best = best.slice(Math.max(0, best.length - BEST_SIZE - 1));
-  localStorage.best = JSON.stringify(best);
-}
-
-var initialGeneration = nGen;
-function nextGeneration() {
-  console.log('Starting generation ' + ++nGen);
-  console.log(best);
-  localStorage.generations = nGen - 1;
-
-  // refresh every other generation to fix incremental slowdown
-  if((nGen - initialGeneration) % 2 === 0)
-    location.reload();
-
-  populate();
-}
-
-window.addEventListener('message', function(e) {
-  var data = JSON.parse(e.data);
-  if(typeof data.from === 'undefined') return;
-
-  if(!data.ready) {
-    var score = data.speed * SPEED_WEIGHT + data.distance;
-    if(best.length < BEST_SIZE || score > best[0].score) {
-      console.log('Adding to set of best sequences');
-      best.push({ genotype: new Genotype(data.g), score: score });
-      updateBest();
-    }
-  }
-
-  if(generation.length === 0) {
-    nextGeneration();
-
-  } else {
-    var g = generation.shift();
-
-    window.postMessage(JSON.stringify({
-      g: g,
-      to: data.from
-    }), '*');
-  }
-}, false);
-
-window.onload = function() {
-  var container = document.getElementById('workers');
-  for(var i = 0; i < WORKERS; i++) {
-      var iframe = document.createElement('iframe');
-      iframe.src = 'worker.html#' + i;
-      container.appendChild(iframe);
-      iframes[i] = iframe.contentWindow;
-  }
-
-  nextGeneration();
-};
--- js/qwop.js
+++ js/qwop.js
@@ -2,35 +2,41 @@
 
 window.qwop = {};
 
-(function(qwop) {
+(function (qwop) {
   var stage;
   var initialized = false, loaded = false;
   var wasDead = false;
 
-  qwop.isAlive = function() {
+  qwop.stage = stage
+
+  qwop.load = function () {
+    runViewer('../athletics.swf')
+  }
+
+  qwop.isAlive = function () {
     // a little hacky, searches through stage for dialog box
-    for(var i = 0; i < stage._children.length; i++) {
+    for (var i = 0; i < stage._children.length; i++) {
       var child = stage._children[i];
-      if(child.x === 2600 || child.x === 2520)
+      if (child.x === 2600 || child.x === 2520)
         return false;
     }
     return true;
   }
 
   var distanceObj;
-  var getDistance = qwop.getDistance = function() {
-    if(!distanceObj) distanceObj = stage.getChildByName('instance6');
+  var getDistance = qwop.getDistance = function () {
+    if (!distanceObj) distanceObj = stage.getChildByName('instance6');
     var text = distanceObj.text;
     return +text.split(' ')[0];
   }
 
-  qwop.onReady = function() {}
+  qwop.onReady = function () {}
 
-  qwop.onStart = function() {}
+  qwop.onStart = function () {}
 
-  qwop.onDeath = function() {}
+  qwop.onDeath = function () {}
 
-  qwop.onLoad = function() {
+  qwop.onLoad = function () {
     loaded = true;
 
     window.removeEventListener('keypress', ShumwayKeyboardListener);
@@ -40,28 +46,28 @@ window.qwop = {};
     qwop.reset();
   };
 
-  qwop.onFrame = function() {}
+  qwop.onFrame = function () {}
 
-  qwop.frameHandler = function() {
-    if(!qwop.isAlive()) {
-      if(initialized && !wasDead) {
+  qwop.frameHandler = function () {
+    if (!qwop.isAlive()) {
+      if (initialized && !wasDead) {
         wasDead = true;
         qwop.onDeath();
       }
-    } else if(!initialized) {
+    } else if (!initialized) {
       initialized = true;
     } else {
       qwop.onFrame();
     }
   };
 
-  qwop.onStageInitialized = function(_stage) {
+  qwop.onStageInitialized = function (_stage) {
     stage = _stage;
-    stage._frameRate = 300;
+    // stage._frameRate = 300;
     qwop.onReady();
   };
 
-  qwop.key = function(char, up) {
+  qwop.key = function (char, up) {
     var code = (char || '_').toUpperCase().charCodeAt(0);
     stage._dispatchEvent(new flash.events.KeyboardEvent(
       up ? 'keyUp' : 'keyDown',
@@ -72,16 +78,16 @@ window.qwop = {};
     ));
   }
 
-  qwop.reset = function() {
-    if(!initialized) {
+  qwop.reset = function () {
+    if (!initialized) {
       stage._mouseTarget._dispatchEvent('click');
     } else {
       qwop.key(' ');
     }
 
-    if(loaded) {
+    if (loaded) {
       wasDead = false;
       qwop.onStart();
     }
   }
-})(window.qwop);
+})(window.qwop)
