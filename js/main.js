6c919fc4cb230b4d81dd3521835b607c31a2812e
Judd Keppel <kepzorz@gmail.com>
2016-01-20 19:41:27 -0800

    initialize reinforcement learner

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   qwop.load()
+  var agent = initAgent()
 
   qwop.onReady = function () {
     console.log('ready')
@@ -9,9 +10,45 @@ window.onload = function () {
     console.log('start')
   }
 
-  qwop.onFrame = function () {}
+  qwop.onFrame = function (e) {
+    var state = getState(e)
+    var action = actions[agent.act(state)]
+    agent.learn(qwop.getDistance()) // meters traveled. higher is better.
+  }
 
   qwop.onDeath = function () {
     console.log('death')
   }
+
+}
+
+function press (char) {}
+  qwop.key('q', false)
+  qwop.key('w', false)
+  qwop.key('o', false)
+  qwop.key('p', false)
+  if(char) qwop.key(char, true)
+}
+
+var actions = [
+  press, // press nothing
+  press.bind(null, 'q'),
+  press.bind(null, 'w'),
+  press.bind(null, 'o'),
+  press.bind(null, 'p')
+]
+
+function getState () {
+  return []
+}
+
+function initAgent () {
+  var env = {}
+  env.getNumStates = function () { return }
+  env.getMaxNumActions = function () { return actions.length }
+
+  var spec = { alpha: 0.01 }
+  agent = new RL.DQNAgent(env, spec)
+
+  return agent
 }
